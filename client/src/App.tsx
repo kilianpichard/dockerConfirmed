beb296bbf1dc5c9b3c0f487e2aa3f927a2b3ea4e
Kilian Pichard <kilian.pichard@canal-plus.com>
2023-09-04 15:37:30 +0200

    feat: Add environment mode and backend API URL to App component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ function App() {
 
     const [message, setMessage] = React.useState('');
     const SERVER_URL = process.env.REACT_APP_SERVER_URL;
+    const {NODE_ENV} = process.env;
 
     const fetchMessage = () => {
         fetch(`${SERVER_URL}/messages/random`)
@@ -21,6 +22,11 @@ function App() {
       <header className="App-header">
         <p>
             {message}
+        </p>
+        <p>
+            You are running this application in <b>{NODE_ENV}</b> mode.
+        <br />
+            The backend API URL is: <b>{SERVER_URL}</b>
         </p>
       </header>
     </div>
