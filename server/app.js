ac163a2eb0369d8dad006aae895712541895a1f2
Kilian Pichard <kilian.pichard@canal-plus.com>
2023-09-04 11:10:34 +0200

    feat: add cors and mongo connection form server

--- server/app.js
+++ server/app.js
@@ -1,6 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const cors = require('cors')
+
+app.use(cors(
+    {
+        origin: 'http://localhost:3001',
+    }
+))
+
+//connect to local mongoDB
+const mongoose = require('mongoose')
+mongoose.connect('mongodb://mongo:27017/docker-confirmed', {useNewUrlParser: true, useUnifiedTopology: true})
+const db = mongoose.connection
+db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+db.on('connected', () => {
+    console.log('MongoDB connected!')
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
