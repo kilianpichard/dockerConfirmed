ee1e618366e660393666ded8a262525d05141989
Kilian Pichard <kilian.pichard@canal-plus.com>
2023-09-06 11:50:00 +0200

    refacto: logging statements and remove unnecessary code

--- server/app.js
+++ server/app.js
@@ -5,13 +5,11 @@ const bodyParser = require('body-parser')
 require('dotenv').config()
 const {MONGO_URI,SERVER_PORT, NODE_ENV} = process.env
 
-console.log('MONGO_URI: ', MONGO_URI)
-
 const morgan = require("morgan");
 const logger = require("./logger");
 
 
-app.use(morgan(":method :url :status :res[content-length] - :response-time ms", { stream: logger.stream }));
+app.use(morgan(":method :url :status :res[content-length] - :response-time ms"));
 
 app.use(cors(
     {
@@ -30,7 +28,7 @@ mongoose.connect(MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true})
 const db = mongoose.connection
 db.on('error', console.error.bind(console, 'MongoDB connection error:'))
 db.on('connected', () => {
-    logger.info('MongoDB connected')
+    console.log('MongoDB connected')
 });
 
 app.get('/', (req, res) => {
@@ -41,5 +39,5 @@ const messageRouter = require('./routes/message.route')
 app.use('/messages', messageRouter)
 
 app.listen(SERVER_PORT, () => {
-  logger.info(`Docker Confirmed server running on port: ${SERVER_PORT}`)
+  console.log(`Docker Confirmed server running on port: ${SERVER_PORT}`)
 })
